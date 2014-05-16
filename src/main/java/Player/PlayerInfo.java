00a22f62c2ab953ffc766b8a203be48fa38ceded
cargt3 <cargt3@gmail.com>
2014-05-16 18:50:56 +0200

    Merge origin/master
    Conflicts:
    	src/pro1/Pro1.java

--- src/main/java/Player/PlayerInfo.java
+++ src/main/java/Player/PlayerInfo.java
@@ -6,10 +6,46 @@
 
 package Player;
 
+import java.io.Serializable;
+
 /**
  *
  * @author Karol
  */
-public class PlayerInfo  {
+public class PlayerInfo extends Player implements Serializable //extends Player 
+{
+    //protected  String nick;
+        //private final String password;  
+    //protected  int id; //!!!!!!!!! TODO unikalny
     
+//    public int getId()
+//    {
+//        return id;
+//    }
+    public PlayerInfo()
+    {
+        super();
+    }
+    
+    public PlayerInfo(Player player)
+    {
+        this.nick = player.getNick();
+        this.id = player.getId();
+        //super(player.getNick(), player.getId());
+    }
+    
+    public PlayerInfo(String nick, int id)
+    {
+        //super(nick, id);
+        this.nick = nick;
+        this.id = id;
+//            this.nick = nick;
+//            id = idNext++;
+        //this.password = password;
+    }
+    @Override
+    public String toString()
+    {
+        return id + " " + nick;
+    }
 }
